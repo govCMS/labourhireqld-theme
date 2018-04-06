7e078ed5a27b64b54d3e70a01370530dc032857a
lnwhsq <38019642+lnwhsq@users.noreply.github.com>
2018-04-06 12:17:37 +1000

    Add files via upload

--- labourhire/templates/labourhirefooter.tpl.php
+++ labourhire/templates/labourhirefooter.tpl.php
@@ -95,12 +95,12 @@ $113(document).ready(function(){
 </script>
 
 
-
-
 <!-- end the modals for feedback form -->
 
+<!-- linked in tracking code -->
+<script type="text/javascript"> _linkedin_data_partner_id = "212667"; </script><script type="text/javascript"> (function(){var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(); </script> <noscript> <img height="1" width="1" style="display:none;" alt="" src="https://dc.ads.linkedin.com/collect/?pid=212667&fmt=gif" /> </noscript>
 
-
+<!-- end linked in tracking code -->
 
 
 
