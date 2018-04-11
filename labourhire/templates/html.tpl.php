d73f454886c539bb18e6a1cfc0d41419be7fbcfb
lnwhsq <38019642+lnwhsq@users.noreply.github.com>
2018-04-11 12:51:50 +1000

    Add files via upload

--- labourhire/templates/html.tpl.php
+++ labourhire/templates/html.tpl.php
@@ -25,6 +25,7 @@ if (drupal_is_front_page()) {
   print $head_title; 
 }
   ?></title>
+  <meta name="google-site-verification" content="yctLD5yd26SDJ8DqT438fJzXQEJUP-TKVYuZ7My6LTQ" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
